60c0b9c48e423f8a92f4a33be8390789289a45b6
Jun Kato <i@junkato.jp>
2021-01-24 12:24:40 +0900

    Add some tests

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -6,17 +6,23 @@ import { sortedIndex } from "../lib/";
 
 describe("sortedIndex tests", () => {
   test("numerical array", () => {
-    expect(sortedIndex([1,2,4,5], 0)).toEqual(0);
-    expect(sortedIndex([1,2,4,5], 1)).toEqual(0);
-    expect(sortedIndex([1,2,4,5], 2)).toEqual(1);
-    expect(sortedIndex([1,2,4,5], 3)).toEqual(2);
-    expect(sortedIndex([1,2,4,5], 4)).toEqual(2);
-    expect(sortedIndex([1,2,4,5], 5)).toEqual(3);
-    expect(sortedIndex([1,2,4,5], 6)).toEqual(4);
+    expect(sortedIndex([1, 2, 4, 5], 0)).toEqual(0);
+    expect(sortedIndex([1, 2, 4, 5], 1)).toEqual(0);
+    expect(sortedIndex([1, 2, 4, 5], 2)).toEqual(1);
+    expect(sortedIndex([1, 2, 4, 5], 3)).toEqual(2);
+    expect(sortedIndex([1, 2, 4, 5], 4)).toEqual(2);
+    expect(sortedIndex([1, 2, 4, 5], 5)).toEqual(3);
+    expect(sortedIndex([1, 2, 4, 5], 6)).toEqual(4);
   });
 
   test("generic array", () => {
     const accessor = (s: string) => s.charCodeAt(0);
-    expect(sortedIndex(["a", "b", "c", "e"], "d", accessor)).toEqual(3);
+    expect(sortedIndex(["a", "b", "d", "e"], "0", accessor)).toEqual(0);
+    expect(sortedIndex(["a", "b", "d", "e"], "a", accessor)).toEqual(0);
+    expect(sortedIndex(["a", "b", "d", "e"], "b", accessor)).toEqual(1);
+    expect(sortedIndex(["a", "b", "d", "e"], "c", accessor)).toEqual(2);
+    expect(sortedIndex(["a", "b", "d", "e"], "d", accessor)).toEqual(2);
+    expect(sortedIndex(["a", "b", "d", "e"], "e", accessor)).toEqual(3);
+    expect(sortedIndex(["a", "b", "d", "e"], "f", accessor)).toEqual(4);
   });
 });
