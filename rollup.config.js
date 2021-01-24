ab064b83990c574fa58451707ea59daf5106fb76
Jun Kato <i@junkato.jp>
2021-01-24 11:24:47 +0900

    Add boilerplate code

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "lib/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "umd",
+        name: "sortedIndex"
+      },
+      {
+        file: pkg.module,
+        format: "es"
+      }
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript"),
+        tsconfig: "tsconfig.json"
+      })
+    ]
+  }
+];
