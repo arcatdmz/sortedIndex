984e3cae67ac1dbcdf72bbd4c843e94b672302c7
Jun Kato <i@junkato.jp>
2021-01-24 11:26:37 +0900

    Add implementation

---
+++ lib/index.ts
@@ -0,0 +1,16 @@
+/**
+ * # sortedindex
+ * 
+ * - 二分探索を行う関数が入った npm パッケージ
+ * - A simple npm package that provides a binary search function for a sorted array
+ * 
+ * ## 使い方 / Usage
+ * 
+ * ```sh
+ * npm install sortedindex
+ * ```
+ * @packageDocumentation
+ * @module sortedindex
+ */
+
+ export { sortedIndex } from "./sortedIndex";
---
+++ lib/sortedIndex.ts
@@ -0,0 +1,24 @@
+/**
+ * 二分探索 / Binary search
+ * 
+ * @param array - 順序付き配列 / Sorted array of objects
+ * @param obj - 配列中のインデックスを探索するオブジェクト / Object to find an appropriate index
+ * @param accessor - オブジェクトの数値表現を返す関数 / Accessor function that returns numeric representation of the object
+ * @returns オブジェクトを挿入すべき位置のインデックス / An appropriate index value in the sorted array to insert the object
+ * @public
+ */
+export function sortedIndex<T>(
+    array: T[],
+    obj: T | number,
+    accessor?: (obj: T) => number
+  ): number {
+    const value = !accessor || typeof obj === "number" ? obj : accessor(obj);
+    let low = 0, high = array.length;
+    while (low < high) {
+      const mid = (low + high) >>> 1;
+      if (accessor(array[mid]) < value) low = mid + 1;
+      else high = mid;
+    }
+    return low;
+  }
+  
