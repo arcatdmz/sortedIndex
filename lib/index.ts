2cd030d0fb7784ff5320936e660f47da3678302e
Jun Kato <i@junkato.jp>
2021-01-24 11:42:18 +0900

    Prettify

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,11 @@
 /**
  * # sortedindex
- * 
+ *
  * - 二分探索を行う関数が入った npm パッケージ
  * - A simple npm package that provides a binary search function for a sorted array
- * 
+ *
  * ## 使い方 / Usage
- * 
+ *
  * ```sh
  * npm install sortedindex
  * ```
@@ -13,4 +13,4 @@
  * @module sortedindex
  */
 
- export { sortedIndex } from "./sortedIndex";
+export { sortedIndex } from "./sortedIndex";
--- lib/sortedIndex.ts
+++ lib/sortedIndex.ts
@@ -1,6 +1,6 @@
 /**
  * 二分探索 / Binary search
- * 
+ *
  * @param array - 順序付き配列 / Sorted array of objects
  * @param obj - 配列中のインデックスを探索するオブジェクト / Object to find an appropriate index
  * @param accessor - オブジェクトの数値表現を返す関数 / Accessor function that returns numeric representation of the object
@@ -8,17 +8,17 @@
  * @public
  */
 export function sortedIndex<T>(
-    array: T[],
-    obj: T | number,
-    accessor?: (obj: T) => number
-  ): number {
-    const value = !accessor || typeof obj === "number" ? obj : accessor(obj);
-    let low = 0, high = array.length;
-    while (low < high) {
-      const mid = (low + high) >>> 1;
-      if (accessor(array[mid]) < value) low = mid + 1;
-      else high = mid;
-    }
-    return low;
+  array: T[],
+  obj: T | number,
+  accessor?: (obj: T) => number
+): number {
+  const value = !accessor || typeof obj === "number" ? obj : accessor(obj);
+  let low = 0,
+    high = array.length;
+  while (low < high) {
+    const mid = (low + high) >>> 1;
+    if (accessor(array[mid]) < value) low = mid + 1;
+    else high = mid;
   }
-  
+  return low;
+}
